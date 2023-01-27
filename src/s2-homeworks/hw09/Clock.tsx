88eaa09dc1ed56c66c3c7fbf02a58d122bdb2e57
My Name <vlad.yahont.98@mail.ru>
2023-01-27 19:22:49 +0300

    commit  hw5 (9.5)

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -38,14 +38,14 @@ function Clock() {
     const stringTime = new Intl.DateTimeFormat('ru', {
         hour: 'numeric',
         minute: 'numeric',
-        second: 'numeric'
-    }).format(date) // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+        second: 'numeric',
+        hour12: true
+    }).format(date) || <br/>// часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
     const stringDate = new Intl.DateTimeFormat('ru').format(date) || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
 
     const stringDay = new Intl.DateTimeFormat("en-US", {weekday: 'long'}).format(date) || <br/>
-    //const stringDay = formatterDate.format(date.getDate()) || <br/> // пишут студенты
     const stringMonth = new Intl.DateTimeFormat("en-US", {month: "long"}).format(date) || <br/> // пишут студенты
 
     return (
